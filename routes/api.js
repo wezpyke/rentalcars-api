1f66e60956f0354af6aa22a6390635eb8d21e38f
Wez Pyke <wez.pyke@gmail.com>
2014-09-04 22:01:47 +0100

    Search for available vehicles

--- routes/api.js
+++ routes/api.js
@@ -122,4 +122,47 @@ router.get('/pickUpOpenTime/:countryName/:cityName/:locationName/:locationID/:dr
     });
 });
 
+
+
+router.get('/dropOffOpenTime/:countryName/:cityName/:locationName/:locationID/:dropOffCountryName/:dropOffCityName/:dropOffLocationID/:dropOffYear/:dropOffMonth/:dropOffDay', function (req, res) {
+    var url = rentalCars.url + encodeURI('<?xml version="1.0" ?><DropOffOpenTimeRQ>' + rentalCars.creds + '<Location id="' + req.params.locationID + '" country="' + req.params.countryName + '" city="' + req.params.cityName + '">' + req.params.locationName + '</Location><Country>' + req.params.dropOffCountryName + '</Country><City>' + req.params.dropOffCityName + '</City><Date year="' + req.params.dropOffYear + '" month="' + req.params.dropOffMonth + '" day="' + req.params.dropOffDay + '"/></DropOffOpenTimeRQ>');
+
+    request(url, {secureProtocol: "SSLv3_method"}, function (error, response, body) {
+        console.log("Body", body);
+        var data = parser.toJson(body, {coerce: true});
+        console.log("To JSON: ", data);
+        var json = JSON.parse(data);
+        var dataToDisplay = json.DropOffOpenTimeRS;
+        console.log(dataToDisplay);
+        res.json(dataToDisplay);
+    });
+});
+
+
+router.get('/search/:countryName/:cityName/:locationName/:locationID/:dropOffCountryName/:dropOffCityName/:dropOffLocationID/:pickUpYear/:pickUpMonth/:pickUpDay/:pickUpHour/:pickUpMinute/:dropOffYear/:dropOffMonth/:dropOffDay/:dropOffHour/:dropOffMinute/:driverAge', function (req, res) {
+    var queryString = '<?xml version="1.0" ?>' +
+                      '<SearchRQ supplierInfo="true">' + rentalCars.creds +
+                        '<PickUp>' +
+                          '<Location id="' + req.params.locationID + '"/>' +
+                          '<Date year="' + req.params.pickUpYear + '" month="' + req.params.pickUpMonth + '" day="' + req.params.pickUpDay + '" hour="' + req.params.pickUpHour + '" minute="' + req.params.pickUpMinute + '"/>' +
+                        '</PickUp>' +
+                        '<DropOff>' +
+                          '<Location id="' + req.params.dropOffLocationID + '"/>' +
+                          '<Date year="' + req.params.dropOffYear + '" month="' + req.params.dropOffMonth + '" day="' + req.params.dropOffDay + '" hour="' + req.params.dropOffHour + '" minute="' + req.params.dropOffMinute + '"/>' +
+                        '</DropOff>' +
+                        '<DriverAge>' + req.params.driverAge + '</DriverAge>' +
+                      '</SearchRQ>';
+    var url = rentalCars.url + encodeURI(queryString);
+
+    request(url, {secureProtocol: "SSLv3_method"}, function (error, response, body) {
+        console.log("Body", body);
+        var data = parser.toJson(body, {coerce: true});
+        console.log("To JSON: ", data);
+        var json = JSON.parse(data);
+        var dataToDisplay = json.SearchRS;
+        console.log(dataToDisplay);
+        res.json(dataToDisplay);
+    });
+});
+
 module.exports = router;
